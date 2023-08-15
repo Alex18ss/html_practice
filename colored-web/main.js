1518e5a4bd5318b424f35dc5274b8191d476f802
Alex18 <131532197+Alex18ss@users.noreply.github.com>
2023-08-15 19:26:22 +0300

    Color-web
    On this website, you can change the body color, text color and text size for preview.

---
+++ colored-web/main.js
@@ -0,0 +1,12 @@
+forText.oninput = function() {
+    text.style.color = forText.value
+}
+
+forBody.oninput = function() {
+    document.body.style.backgroundColor = forBody.value
+}
+
+forTextSize.oninput = function(){
+    text.style.fontSize = forTextSize.value + 'px'
+}
+
