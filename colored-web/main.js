bc509e376dac361b3601ed66a06eb2763ffb1d06
Alex18 <aleka0205@gmail.com>
2023-08-17 18:24:51 +0300

    added local storage

--- colored-web/main.js
+++ colored-web/main.js
@@ -1,12 +1,38 @@
+
+text.style.color = localStorage.getItem('txtColor') || '#000000'
+document.body.style.backgroundColor = localStorage.getItem('bgColor') || '#ffffff'
+text.style.fontSize = localStorage.getItem('txtSize') + 'px' || '16px'
+
+forText.value = localStorage.getItem('txtColor') || '#000000'
+forBody.value = localStorage.getItem('bgColor') || '#ffffff'
+forTextSize.value = localStorage.getItem('txtSize') || '16'
+
 forText.oninput = function() {
     text.style.color = forText.value
+
 }
 
 forBody.oninput = function() {
     document.body.style.backgroundColor = forBody.value
+
 }
 
 forTextSize.oninput = function(){
     text.style.fontSize = forTextSize.value + 'px'
+
 }
 
+forText.onchange = function() {
+    localStorage.setItem('txtColor', forText.value)
+
+}
+
+forBody.onchange = function() {
+    localStorage.setItem('bgColor', forBody.value)
+
+}
+
+forTextSize.onchange = function(){
+    localStorage.setItem('txtSize', forTextSize.value)
+
+}
